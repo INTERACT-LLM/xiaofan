ef5ef9d11c57f2f9e918f9771c2a7048543472be
Ross Deans Kristensen-McLachlan <rdkm@cc.au.dk>
2026-03-24 08:41:45 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite' // Tilføj denne import
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/xiaofan/',
   plugins: [
     vue(),
     vueDevTools(),
